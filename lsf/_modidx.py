a34a534482db76188563c5b663d884681045976b
kaiguender <kai.guender@yahoo.de>
2022-10-12 18:03:44 +0200

    added lsf code

--- lsf/_modidx.py
+++ lsf/_modidx.py
@@ -5,4 +5,9 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://kaiguender.github.io',
                 'git_url': 'https://github.com/kaiguender/lsf',
                 'lib_path': 'lsf'},
-  'syms': {'lsf.core': {'lsf.core.foo': ('core.html#foo', 'lsf/core.py')}}}
+  'syms': { 'lsf.core': { 'lsf.core.BaseWeightsBasedPredictor': ('core.html#baseweightsbasedpredictor', 'lsf/core.py'),
+                          'lsf.core.BaseWeightsBasedPredictor.__init__': ('core.html#baseweightsbasedpredictor.__init__', 'lsf/core.py'),
+                          'lsf.core.BaseWeightsBasedPredictor.fit': ('core.html#baseweightsbasedpredictor.fit', 'lsf/core.py'),
+                          'lsf.core.BaseWeightsBasedPredictor.getWeightsData': ( 'core.html#baseweightsbasedpredictor.getweightsdata',
+                                                                                 'lsf/core.py'),
+                          'lsf.core.BaseWeightsBasedPredictor.predict': ('core.html#baseweightsbasedpredictor.predict', 'lsf/core.py')}}}
